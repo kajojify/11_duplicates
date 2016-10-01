f8af05f8fa8678559226f49c1fc98d7a223a5f54
Miroslav Vinogradov <rsamoilenko@mail.ru>
2016-10-01 18:29:03 +0300

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,36 @@
+import os
+from os.path import isdir, getsize
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def get_duplicates(directory_path):
+    """Производит поиск файлов-дубликатов в директории directory_path.
+    Возвращает словарь, где ключ - это название файла-дубликата,
+    а значение - список директорий, в которых находятся файлы-дубликаты
+    с таким названием."""
+
+    duplicates = dict()
+    for current_dir, dir_list, file_list in os.walk(directory_path):
+        for file in file_list:
+            if file not in duplicates:
+                duplicates[file] = [current_dir]
+            else:
+                sample_file_name = os.path.join(duplicates[file][0], file)
+                current_file_name = os.path.join(current_dir, file)
+                if getsize(current_file_name) == getsize(sample_file_name):
+                    duplicates[file].append(current_dir)
+    for key in list(duplicates):
+        if len(duplicates[key]) == 1:
+            duplicates.pop(key)
+    return duplicates
 
 
 if __name__ == '__main__':
-    pass
+    directory_path = input("В какой директории будем искать дубликаты? --- ")
+    if not isdir(directory_path):
+        print("Нет директории с таким именем! Завершение программы.")
+        exit()
+    d = get_duplicates(directory_path)
+    for file, dirs in d.items():
+        print("\nДанные файлы являются дубликатами:\n")
+        for dir in dirs:
+            print("%s/%s" % (dir, file))
